07f9e9da53b0d4846d419d078ddc8276ba236b18
Mateus Sandes <mateus.sandes@liferay.com>
2023-02-11 11:06:45 -0300

    Entendendo variaveis

---
+++ java-script/VarJS.js
@@ -0,0 +1,15 @@
+//o javascript ja faz as conversoes dos dados para os seus respectivos tipos;
+
+//strings
+var nome = "Hello world!";
+document.write(nome);
+
+//number
+var numInt = 45;
+var num2 = 45.34; //nao precisa dos tipos para declarar variaveis
+alert(num2 + numInt); //exibindo valores em JavaScript
+
+//booleano
+var teste = false;
+
+console.log(nome)
